1aba2e1508e4c532e5b21edf8902f2c2a63d883d
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-05-11 15:37:03 +0530

    feat: add home page boilder code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages";
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/mockman" element={<Mockman />} />
       </Routes>
     </>

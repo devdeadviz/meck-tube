d4084e10df8ab2258960bc5e9daf8f0866053aae
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-05-12 15:29:16 +0530

    fix: move navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Mockman from "mockman-js";
 import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
 import { Home } from "./pages";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mockman" element={<Mockman />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,7 @@
-import { Navbar } from "../../components";
 import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
+  return <div></div>;
 };
 
 export { Home };

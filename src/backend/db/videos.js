3d3c85d1d689dc4b817b9042d79c752e5d242457
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-05-14 19:16:00 +0530

    db: update videos

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,113 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "GxS30X_unjU",
+    title: "Buyers Guide : What Size Mechanical Keyboard Should you Get?",
+    creator: "TaeKeyboards",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Today we're going to compare the different mechanical keyboard sizes and form factors, which should help beginners recognise what's actually available on the market.",
+    duration: "8:31",
+    view: "1M",
+    uploadedOn: "30 Jan 2020",
+    categoryName: "Buyers Guide",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "s6SY7AQsyI0",
+    title: "Guide to Mechanical Keyboards for Beginners!",
+    creator: "gonewiththewendy",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "What is a mechanical keyboard? How does it differ from a membrane one? What are the different types of switches? I'll be going over all these questions in this mechanical keyboard video for beginners!",
+    duration: "9:33",
+    view: "419K",
+    uploadedOn: "20 Sept 2020",
+    categoryName: "Buyers Guide",
+  },
+  {
+    _id: "B8jX2DpHA5E",
+    title: "Mechanical keyboards: everything you need to know",
+    creator: "TechRadar",
+    description:
+      "Mechanical keyboards have exploded in popularity over the last few years. Chances are that if you haven't already bought one (or at least thought about it) one of your friends has.",
+    duration: "6:41",
+    view: "746K",
+    uploadedOn: "15 Aug 2016",
+    categoryName: "Buyers Guide",
+  },
+  {
+    _id: "GxS30X_unjU",
+    title: "Buyers Guide : What Size Mechanical Keyboard Should you Get?",
+    creator: "TaeKeyboards",
+    description:
+      "Today we're going to compare the different mechanical keyboard sizes and form factors, which should help beginners recognise what's actually available on the market.",
+    duration: "8:31",
+    view: "1M",
+    uploadedOn: "30 Jan 2020",
+    categoryName: "Buyers Guide",
+  },
+  {
+    _id: "-Ln9rA_usoY",
+    title: "How to Choose the Perfect Switch For YOU!",
+    creator: "Switch and Click",
+    description:
+      "Finding a switch for your mechanical keyboard can be a difficult process. We'll tell you exactly what you need to know, so you can find the PERFECT switch for YOU!",
+    duration: "9:23",
+    view: "1M",
+    uploadedOn: "27 Apr 2021",
+    categoryName: "Switch",
+  },
+  {
+    _id: "rm-WuK_9OBM",
+    title: "Mechanical Switch Comparison (Sound Only)",
+    creator: "Mattie Keebs",
+    description:
+      "After many requests, here is a comparison between blue, red, and brown Cherry MX mechanical keyboard switches.  If you liked this video don't forget to leave a thumbs up!  Remember that keyboards, keycaps, desks, and your room acoustics can all change the sound of any given switch.",
+    duration: "2:05",
+    view: "2.4M",
+    uploadedOn: "5 Nov 2019",
+    categoryName: "Switch",
+  },
+  {
+    _id: "WM8SloaOHx4",
+    title: "How to build your FIRST custom keyboard! (BUDGET FRIENDLY)",
+    creator: "Hipyo Tech",
+    description:
+      "In this video, a beginner builds their first Custom Mechanical keyboard with no experience. This is a budget beginner build with the Idobao Id67 with Gateron Milky Yellows and XDA Bee Keycaps.",
+    duration: "17:41",
+    view: "1.8M",
+    uploadedOn: "25 July 2021",
+    categoryName: "Custom",
+  },
+  {
+    _id: "GxS30X_unjU",
+    title: "Buyers Guide : What Size Mechanical Keyboard Should you Get?",
+    creator: "TaeKeyboards",
+    description:
+      "Today we're going to compare the different mechanical keyboard sizes and form factors, which should help beginners recognise what's actually available on the market.",
+    duration: "8:31",
+    view: "1M",
+    uploadedOn: "30 Jan 2020",
+    categoryName: "Buyers Guide",
+  },
+  {
+    _id: "GxS30X_unjU",
+    title: "Buyers Guide : What Size Mechanical Keyboard Should you Get?",
+    creator: "TaeKeyboards",
+    description:
+      "Today we're going to compare the different mechanical keyboard sizes and form factors, which should help beginners recognise what's actually available on the market.",
+    duration: "8:31",
+    view: "1M",
+    uploadedOn: "30 Jan 2020",
+    categoryName: "Buyers Guide",
+  },
+  {
+    _id: "GxS30X_unjU",
+    title: "Buyers Guide : What Size Mechanical Keyboard Should you Get?",
+    creator: "TaeKeyboards",
+    description:
+      "Today we're going to compare the different mechanical keyboard sizes and form factors, which should help beginners recognise what's actually available on the market.",
+    duration: "8:31",
+    view: "1M",
+    uploadedOn: "30 Jan 2020",
+    categoryName: "Buyers Guide",
   },
 ];

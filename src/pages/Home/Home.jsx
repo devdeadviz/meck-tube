dbd6e78f0f20fdccdca0e3bdc0d09b44709382bd
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-20 18:14:13 +0530

    refactor: remove extra spacing and add Chip component with demo text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { VideoCard } from "../../components";
+import { Chip, VideoCard } from "../../components";
 import { getVideos } from "../../services";
 import "./Home.css";
 
@@ -15,17 +15,22 @@ const Home = () => {
 
   return (
     <div className="home-wrapper">
-      {videos.map(({ _id, duration, title, creator, view, uploadedOn }) => (
-        <VideoCard
-          key={_id}
-          _id={_id}
-          duration={duration}
-          title={title}
-          creator={creator}
-          view={view}
-          uploadedOn={uploadedOn}
-        />
-      ))}
+      <div className="home-category-wrapper flex">
+        <Chip chipText={"Hola Dola"} />
+      </div>
+      <div className="flex flexWrap flexJustifyCenter">
+        {videos.map(({ _id, duration, title, creator, view, uploadedOn }) => (
+          <VideoCard
+            key={_id}
+            _id={_id}
+            duration={duration}
+            title={title}
+            creator={creator}
+            view={view}
+            uploadedOn={uploadedOn}
+          />
+        ))}
+      </div>
     </div>
   );
 };

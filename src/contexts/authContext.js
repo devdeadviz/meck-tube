9ef04c4bf34c7d84761bb37353a0052d3499a6e9
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-22 17:16:05 +0530

    feat: add auth reducer function

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext } from "react";
+import { authReducer } from "../reducers";
 
 const initialAuthValue = {
   authenticated: !!JSON.parse(localStorage.getItem("user")),

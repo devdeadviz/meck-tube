f3ed6e622982cb54e469dc7518bedcb350ffa119
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-23 15:31:12 +0530

    fix: signup service

--- src/services/auth/signup.js
+++ src/services/auth/signup.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const signup = async (user) => {
   try {
-    const { data } = axios.post("/api/auth/signup", user);
+    const { data } = await axios.post("/api/auth/signup", user);
     return data;
   } catch (error) {
     console.error(error.response.data);

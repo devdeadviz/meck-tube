ed84f679e7becb7a1f84acd0ba4fbeb9d4b69d59
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-06-25 01:55:08 +0530

    feat: add like video services

--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,4 @@ export { getVideos } from "./video/getVideos";
 export { getCategories } from "./video/getCategories";
 export { login } from "./auth/login";
 export { signup } from "./auth/signup";
+export { likeVideo, deleteLikeVideo } from "./video/likeService";
---
+++ src/services/video/likeService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const HEADERS = { authorization: localStorage.getItem("encodedToken") };
+
+const likeVideo = async (video) => {
+  try {
+    const { data } = await axios.post("/api/user/likes", video, {
+      headers: HEADERS,
+    });
+    return data;
+  } catch (error) {
+    console.error(error.response.data);
+  }
+};
+
+const deleteLikeVideo = async (videoId) => {
+  try {
+    const { data } = await axios.delete(`/api/user/likes/${videoId}`, {
+      headers: HEADERS,
+    });
+    return data;
+  } catch (error) {
+    console.error(error.response.data);
+  }
+};
+
+export { likeVideo, deleteLikeVideo };

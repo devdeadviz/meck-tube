0b1592e3358016055f8fc88fdad9a06554d8c577
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-07-15 17:07:51 +0530

    feat: add create playlist functionality

--- src/components/PlaylistModal/PlaylistModal.jsx
+++ src/components/PlaylistModal/PlaylistModal.jsx
@@ -1,9 +1,18 @@
 import "./PlaylistModal.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { useState } from "react";
+import { createNewPlaylist } from "../../services";
 
 const PlaylistModal = ({ setShowPlaylistModal }) => {
   const [showPlaylistInput, setShowPlaylistInput] = useState(false);
+  const [playlistTitleInput, setPlaylistTitleInput] = useState("");
+
+  const createNewPlaylistHandler = async (playlist) => {
+    const { playlists } = await createNewPlaylist(playlist);
+    setPlaylistTitleInput("");
+    setShowPlaylistInput(false);
+    console.log("Playlist from server", playlists);
+  };
 
   return (
     <div className="playlist-modal">
@@ -41,8 +50,21 @@ const PlaylistModal = ({ setShowPlaylistModal }) => {
                 className="text-input playlist-name-input my-0"
                 name="name"
                 placeholder="Enter the playlist name"
+                value={playlistTitleInput}
+                onChange={(e) => setPlaylistTitleInput(e.target.value)}
               />
-              <button type="button" class="btn btn-primary create-btn mx-2">Create</button>
+              <button
+                type="button"
+                className="btn btn-primary create-btn mx-2"
+                onClick={() =>
+                  createNewPlaylistHandler({
+                    title: playlistTitleInput,
+                    description: "",
+                  })
+                }
+              >
+                Create
+              </button>
             </section>
           )}
           {!showPlaylistInput && (
